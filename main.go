5dbb46d59269d61e47431e54178c36d98788f3b6
Distortions <33408758+Distortions81@users.noreply.github.com>
2021-11-10 12:56:47 -0700

    variable size checkerboard bg

--- main.go
+++ main.go
@@ -13,9 +13,28 @@ var outputFile = "output.png"
 func main() {
 
 	image := image.NewRGBA(image.Rect(0, 0, 1024, 1024))
+	var c uint8 = 0
+	size := 16
+
 	for y := 0; y < 1024; y++ {
+		if y%size == 0 {
+			if c == 0 {
+				c = 64
+			} else {
+				c = 0
+			}
+		}
 		for x := 0; x < 1024; x++ {
-			image.Set(x, y, color.RGBA{uint8(x), uint8(y), 0, 255})
+
+			if x%size == 0 {
+				if c == 0 {
+					c = 64
+				} else {
+					c = 0
+				}
+			}
+
+			image.Set(x, y, color.RGBA{c, c, c, 255})
 		}
 	}
 
