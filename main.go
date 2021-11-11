2662d4755ae9404258cfa1a60edbcef7ff32b145
Distortions <33408758+Distortions81@users.noreply.github.com>
2021-11-10 22:33:55 -0700

    decode and draw bp

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/zlib"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/color"
@@ -41,10 +42,55 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(enflated))
+	//fmt.Println(string(enflated))
+
+	type Xy struct {
+		X float32
+		Y float32
+	}
+	type Ent struct {
+		Entity_number int
+		Name          string
+		Position      Xy
+		Direction     int
+	}
+	type BpData struct {
+		Entities []Ent
+		Item     string
+		Label    string
+		Version  int64
+	}
+	type Bp struct {
+		BluePrint BpData
+	}
+
+	newbp := Bp{}
+
+	err = json.Unmarshal(enflated, &newbp)
+	if err != nil {
+		panic(err)
+	}
+
+	if 1 == 2 {
+		str, err := json.Marshal(newbp)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(str))
+	}
 
 	mapimage := image.NewRGBA(image.Rect(0, 0, 1024, 1024))
 	newimage := image.NewRGBA(image.Rect(0, 0, 1024, 1024))
+
+	count := 0
+	for _, v := range newbp.BluePrint.Entities {
+		x := int(v.Position.X)
+		y := int(v.Position.Y)
+		mapimage.Set(x, y, color.RGBA{255, 255, 255, 255})
+
+		count = count + 1
+	}
+	fmt.Println(count)
 
 	var c uint8 = 0
 	size := 16
@@ -52,7 +98,7 @@ func main() {
 	for y := 0; y < 1024; y++ {
 		if y%size == 0 {
 			if c == 0 {
-				c = 64
+				c = 8
 			} else {
 				c = 0
 			}
@@ -61,7 +107,7 @@ func main() {
 
 			if x%size == 0 {
 				if c == 0 {
-					c = 64
+					c = 8
 				} else {
 					c = 0
 				}
@@ -71,7 +117,7 @@ func main() {
 			if mapimage.At(x, y) != (color.RGBA{0, 0, 0, 0}) {
 				newimage.Set(x, y, mapimage.At(x, y))
 			} else {
-				newimage.Set(x, y, color.RGBA{c, c, c, 255})
+				newimage.Set(x, y, color.RGBA{c, 0, 0, 255})
 			}
 		}
 	}
